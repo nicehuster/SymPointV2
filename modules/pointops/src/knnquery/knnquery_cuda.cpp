0ebade0190241d062c6584475c7d9167aabd9c25
muly <mulyoved@gmail.com>
2026-08-02 13:35:40 +0000

    fix: support 256-neighbor KNN queries

--- modules/pointops/src/knnquery/knnquery_cuda.cpp
+++ modules/pointops/src/knnquery/knnquery_cuda.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <THC/THC.h>
+#include <torch/extension.h>
 #include <torch/serialize/tensor.h>
 #include <ATen/cuda/CUDAContext.h>
 #include "knnquery_cuda_kernel.h"
@@ -7,6 +8,13 @@
 
 void knnquery_cuda(int m, int nsample, at::Tensor xyz_tensor, at::Tensor new_xyz_tensor, at::Tensor offset_tensor, at::Tensor new_offset_tensor, at::Tensor idx_tensor, at::Tensor dist2_tensor)
 {
+    TORCH_CHECK(
+        nsample >= 1 && nsample <= KNNQUERY_MAX_NEIGHBORS,
+        "knnquery nsample must be between 1 and ",
+        KNNQUERY_MAX_NEIGHBORS,
+        "; got ",
+        nsample
+    );
     const float *xyz = xyz_tensor.data_ptr<float>();
     const float *new_xyz = new_xyz_tensor.data_ptr<float>();
     const int *offset = offset_tensor.data_ptr<int>();
